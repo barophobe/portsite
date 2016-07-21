0ebbcc4b92b95a5b57bfdf8cb69367d077e842f8
barophobe <edoconnolly@gmail.com>
2016-07-21 01:07:53 +0100

    first commit

---
+++ contact_me.php
@@ -0,0 +1,105 @@
+
+
+<div class="container-fluid bodyBkg">
+<div class="row">
+<div class="col-lg-5 col-lg-offset-1 col-md-6 col-sm-12">
+  
+
+<p class="conTxt">Please feel free to contact me and I will get back to you<span class="frmConHd2">&nbsp;!</span></p>
+<?php if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  //form validation
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message']) ) {
+  
+    // Create the body:
+    $body = "Name: {$_POST['name']}\n\nmessage: {$_POST['message']}";
+    $body = wordwrap($body, 200);
+  
+    // Send the email:
+    mail('edoconnolly@gmail.com', 'Contact Form Submission', $body, "From: {$_POST['email']}");
+
+    // Print a message:
+    echo '<p class="conTxt">Your message has been sent<span class="frmConHd2">&nbsp;!</span></p>';
+    
+    // Clear $_POST (so that the form's not sticky):
+    $_POST = array();
+  
+  } else {
+    echo '<p style="font-weight: bold; color: #C00">Please fill out the form completely.</p>';
+  } 
+
+}// end of main isset() IF.
+?>
+
+
+
+
+    <div class="row">
+
+          <div class="socL">
+
+            <ul class="landList list-inline" >
+                 <li><a href="https://www.facebook.com/ed.connolly.560"><span class="hovF"></span></a></li>
+                  <li><a href="https://twitter.com/edddddd"><span class="hovT"></span></a></li>
+                  <li><a href="https://www.linkedin.com/in/edmond-connolly-6b2a84116?trk=nav_responsive_tab_profile"><span class="hovL"></span></a></li>
+                
+            
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+  <div class="col-lg-6 col-md-6 col-sm-12">
+    
+
+<form class="form-horizontal" action="index.php" method="post">
+  <fieldset>
+    <div class="row">
+<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+<h2 class="frmConHd">Contact <span class="frmConHd2">me</span>:</h2>
+</div>
+</div>
+<div class="ftBor">
+  <div class="form-group has-feedback">
+    <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+     <input class="form-control input-lg" type="text" name="name" size="20" maxlength="20" placeholder="Your Name" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>" />
+     <span class="glyphicon glyphicon-user whtIco form-control-feedback" aria-hidden="true"></span>
+    </div>
+    </div>
+
+    <div class="form-group has-feedback">
+      <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+     <input class="form-control input-lg" type="email" name="email" size="30" placeholder="Your Email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" />
+     <span class="glyphicon glyphicon-envelope whtIco form-control-feedback" aria-hidden="true"></span>
+  </div>
+  </div>
+
+    <div class="form-group">
+      <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+     <textarea class="form-control input-lg" rows="7" cols="30" type="text" name="message" placeholder="Your Message" value="<?php if (isset($_POST['message'])) echo $_POST['message']; ?>"  /></textarea> 
+  </div>
+  </div>
+  </div>
+
+                       
+  </fieldset>
+
+  <div class="form-group">
+     <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+     <div align="center"><button class="btn btn-lg" type="submit" name="submit" value="Send!">Send</button></div>
+  </div>
+</div>
+</form>
+
+
+</div>
+
+
+</div>
+
+
+
---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Edc</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="portfolio.css">
+  <link rel="stylesheet" href="media.css">
+	<script   src="https://code.jquery.com/jquery-1.12.4.js"   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="   crossorigin="anonymous"></script>
+  <meta charset="utf-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body id="pageDim">
+  <?php include_once("analyticstracking.php") ?>
+	<header id="hdrBg">
+		<img src="images/ed-connolly-LOGO-03.png" id="logo" alt="">
+	</header>
+	<div class="bio"><h2>Edmond Connolly > about me</h2><p><img src="images/Ed_Connolly_profile_02.png" id="edPic" alt="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam blanditiis labore rem facere consequuntur ipsam ea perspiciatis est aut, explicabo velit quod mollitia voluptatem laudantium magni eum accusantium, provident vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio non perferendis necessitatibus soluta similique porro reiciendis iure assumenda unde animi, quis ullam culpa. Tempora obcaecati, unde deserunt vitae eligendi vero?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur aspernatur numquam neque, unde, quae vitae possimus repellendus nihil pariatur, sequi saepe debitis dolores modi quibusdam iure non sint dolore deleniti.</p><br>
+    <h2>Edmond Connolly > </h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam possimus optio magni sint et vel, placeat! Saepe voluptatum modi aliquid dolorem magnam, facilis quis enim esse voluptate similique autem aut?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quasi at dignissimos ullam, placeat harum soluta minus possimus aperiam, eligendi amet molestiae modi quidem nemo officia debitis maiores adipisci incidunt?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias in animi, excepturi totam dolore eligendi et necessitatibus nemo repellat placeat. Ipsam dolores nam ut nobis, soluta iure consectetur, necessitatibus adipisci.</p><br>
+  <h2>Edmond Connolly > </h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam possimus optio magni sint et vel, placeat! Saepe voluptatum modi aliquid dolorem magnam, facilis quis enim esse voluptate similique autem aut?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quasi at dignissimos ullam, placeat harum soluta minus possimus aperiam, eligendi amet molestiae modi quidem nemo officia debitis maiores adipisci incidunt?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias in animi, excepturi totam dolore eligendi et necessitatibus nemo repellat placeat. Ipsam dolores nam ut nobis, soluta iure consectetur, necessitatibus adipisci.</p></div>
+	
+
+
+<div class="container-fluid">
+  
+  <div class="row-fluid senter">
+    <div class="jumbotron"><h1 id="portHd">Portfolio</h1></div>
+  </div>
+</div>
+
+
+
+
+
+<div class="tabbable"> <!-- Only required for left/right tabs -->
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#tab1" data-toggle="tab">1</a></li>
+    <li><a href="#tab2" data-toggle="tab">2</a></li>
+    
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="tab1">
+      <div id="effect-1" class="effects clearfix">
+    <div class="img  col-sm-6 col-xs-6 col-md-6">
+        <h3 class="portH">Info Pages-Unipupil</h3><p class="projSum">This project involved the development of several information pages for an already existing educatiional portal. A cross functional team including myself a Project Manager and a Designer collaborated with the client and produced 5 pages.  The framework used was Twitter bootstrap. It is a mobile first responsive website.</p>
+        <div class="overlay">
+          <h5 class="overHead">bootstrap/Html<br>css/javascript</h5>
+          <h3 class="overHead3">Unipupil<br>Info Pages</h3>
+            <a href="https://github.com/barophobe/Dsa-Major-Project" class="expand" target="_blank">Git Repo</a><a href="http://edcodev.com/portfolio/major_proj/About_us.html" class="expand1" target="_blank">Visit Site</a>
+            <a class="close-overlay hidden">x</a>
+        
+    
+    </div></div><div class="img col-sm-6 col-xs-6 col-md-6 sec ">
+         <h3 class="portH">Landing Pages-Unipupil</h3><p class="projSum">Two landing pages were developed for Unipupil targeted at distinct user types of the portal, namely Educational Institutions and prospective students. As before a cross functional team liased with the client and developed a design which i then developed. This totalled two landing pages and two thank you pages.Form data was captured via mailchimp.</p>
+        <div class="overlay">
+          <h5 class="overHead">bootstrap/Html<br>css/javascript</h5>
+          <h3 class="overHead3">Unipupil<br>Landing Pages</h3>
+            <a href="https://github.com/barophobe/Landing-pages-Unipupil" class="expand" target="_blank">Git Repo</a><a href="http://edcodev.com/portfolio/Unipupil%20landing%20page/Institutions_landing_page.html" class="expand1" target="_blank">Visit Site</a>
+            <a class="close-overlay hidden">x</a>
+       
+    
+     </div></div><div class="img col-sm-6 col-xs-6 col-md-6">
+         <h3 class="portH">Jquery Mobile SPA</h3><p class="projSum">A simple single page application creating dynamic pages from json data. A jquery Ajax method is used to bring the data into the app and a function sorts a list alphabetically.</p>
+        <div class="overlay">
+           <h5 class="overHead">Jquery Mobile<br>Json/Ajax</h5>
+          <h3 class="overHead3">Single Page<br>Application</h3>
+            <a href="https://github.com/barophobe/Jquery-mobile-json-dynamic-pages-sort-alphabetically" class="expand" target="_blank">Git Repo</a><a href="#" class="expand1" target="_blank">Visit Site</a>>
+            <a class="close-overlay hidden">x</a>
+     
+   
+        </div></div><div class="img col-sm-6 col-xs-6 col-md-6 sec">
+         <h3 class="portH">Angular To Do List</h3><p class="projSum">Simple to do list, with function to delete a to do, strike it off as being done and also high light its importance. </p>
+        <div class="overlay">
+           <h5 class="overHead">AngularJs<br>css</h5>
+          <h3 class="overHead3">To Do List</h3>
+            <a href="https://github.com/barophobe/Angular-to-Do-list" class="expand" target="_blank">Git Repo</a><a href="#" class="expand1" target="_blank">Visit Site</a>
+            <a class="close-overlay hidden">x</a>
+     
+       
+</div></div></div>
+
+
+
+    </div>
+    <div class="tab-pane" id="tab2">
+     <div id="effect-1" class="effects clearfix">
+    <div class="img col-sm-6 col-xs-6 col-md-6">
+        <h3
+        class="portH">HTML5 and Css animations and more</h3><p class="projSum">Some advanced features of html5 and css3. Including drag and drop, animations, transforms and canvas examples. Sass preprocessor mixins, inheritance, variables and nesting.</p>
+        <div class="overlay">
+          <h5 class="overHead">Html/css<br>Sass/javascript</h5>
+          <h3 class="overHead3">Animations<br>+ Canvas Tag</h3>
+            <a href="https://github.com/barophobe/Html5advancedFeatures" class="expand" target="_blank">Git Repo</a><a href="http://edcodev.com/portfolio/major_proj/About_us.html" class="expand1" target="_blank">Visit Site</a>
+            <a class="close-overlay hidden">x</a>
+        
+    
+    </div></div><div class="img col-sm-6 col-xs-6 col-md-6 sec">
+         <h3
+         class="portH">Html5 -Various Features.</h3><p class="projSum">A few pages to display html5 semantic layout elements. Audio and video features are also displayed along with a Html5 form. There is also a look at the flexbox method of responsive development. This also uses media queries to show how the flexbox items can be reordered for different screen sizes.</p>
+        <div class="overlay">
+          <h5 class="overHead">Html5/css<br>css/javascript</h5>
+          <h3 class="overHead3">Semantic Layout/forms<br>Flexbox/Media</h3>
+            <a href="https://github.com/barophobe/html5Css3-Forms-media-semanticLayoutand-more" class="expand" target="_blank">Git Repo</a><a href="http://edcodev.com/portfolio/Unipupil%20landing%20page/Institutions_landing_page.html" class="expand1" target="_blank">Visit Site</a>
+            <a class="close-overlay hidden">x</a>
+       
+    
+     </div></div><div class="img col-sm-6 col-xs-6 col-md-6">
+         <h3
+         class="portH">Jquery Ajax API(Json)</h3><p class="projSum">Two pages which use Jquery Ajax functions to retrieve Json data from a remote site. The exchange rate api page features significant javascript to display data and to append data on to elements.</p>
+        <div class="overlay">
+           <h5 class="overHead">Jquery<br>Json/Ajax</h5>
+          <h3 class="overHead3">Flikr<br>XRates API</h3>
+            <a href="https://github.com/barophobe/Jquery-get_json-Ajax-flikr-and-exchange-rateApi-" class="expand" target="_blank">Git Repo</a><a href="#" class="expand1" target="_blank">Visit Site</a>>
+            <a class="close-overlay hidden">x</a>
+     
+   
+        </div></div><div class="img col-sm-6 col-xs-6 col-md-6 sec">
+         <h3
+         class="portH">Angular To Do List</h3><p class="projSum">Two pages which use Jquery Ajax functions to retrieve Json data from a remote site. The exchange rate api page features </p>
+        <div class="overlay">
+           <h5 class="overHead">AngularJs<br>css</h5>
+          <h3 class="overHead3">To Do List</h3>
+            <a href="https://github.com/barophobe/Angular-to-Do-list" class="expand" target="_blank">Git Repo</a><a href="#" class="expand1" target="_blank">Visit Site</a>
+            <a class="close-overlay hidden">x</a>
+     
+       
+</div></div></div>
+
+ 
+
+    </div>
+  </div>
+</div>
+ 
+<?php include ('contact_me.php'); ?>
+<footer id="ed">
+<div class="row-fluid">
+&copy;&nbsp;All rights Reseserved Ed connolly
+</div></footer>
+ <script type="text/javascript" src="bootstrap.js"></script>
+   	 <script type="text/javascript" src="portfolio.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"> </script>
+</body>
+</html>
